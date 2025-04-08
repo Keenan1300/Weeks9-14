30d58ca32d92005dcb14bfd55f405790b3baa5fe
hamilt25 <hamilt25@OA-S143-01>
2025-04-08 12:51:51 -0400

    EarlyStagesUFOs

--- Assets/Scripts/SystemsScripts/EnemyScript.cs
+++ Assets/Scripts/SystemsScripts/EnemyScript.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+
+    public float HP;
+    public float GunCoolDown;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,42 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (HP < 1) 
+        {
         
+        }
+
+        Vector3 UFOpos = transform.position;
+        //Vector3 UFOrot = transform.eulerAngles;
+        Vector3 UFOrot = transform.up;
+        transform.eulerAngles = UFOrot;
+        transform.position = UFOpos;
+
+        search();
+    }
+
+
+    void search()
+    {
+        Vector3 UFOrot = transform.up;
+        Vector3 UFOpos = transform.position;
+        UFOrot.z += 5;
+        transform.up = UFOrot;
+    }
+
+    void alerted() 
+    {
+
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mouse - transform.position;
+        transform.up = direction;
+        transform.position -= (direction * Time.deltaTime);
+    }
+
+
+    void death()
+    {
+        //Remove All listeners
+
     }
 }
